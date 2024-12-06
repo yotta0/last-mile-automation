35db6e385524f68ef416c189528d09629a3d130c
Davi Castro <jose.castro@irede.org.br>
2024-12-06 17:11:53 -0300

    feature: adjusted project endpoints, implemented initial login/auth flow

--- src/infra/web_api/routes/user.py
+++ src/infra/web_api/routes/user.py
@@ -1,8 +1,44 @@
-from flask import Blueprint
+from uuid import UUID
+from flask import Blueprint, jsonify, request
+from dependency_injector.wiring import inject, Provide
+
+from src.interface.web.schemas.user import (UserCreateSchema, UserUpdateSchema)
+from src.interface.web.controller.user import UserController
+from src.interface.web.middleware.auth import auth_required
+from src.infra.init.injector import Container
+
 
 user_bp = Blueprint('user', __name__, url_prefix='/api/v1/users')
 
-@user_bp.route('/')
-def get_users():
-    return 'Hello World!'
+@user_bp.route('', methods=['GET'])
+@inject
+@auth_required
+def get_users(user_controller: UserController = Provide[Container.user_controller]):
+    return jsonify(user_controller.get_users())
 
+@user_bp.route('/<uuid:user_id>', methods=['GET'])
+@inject
+@auth_required
+def get_user(user_id: UUID, user_controller: UserController = Provide[Container.user_controller]):
+    return jsonify(user_controller.get_user(user_id))
+
+@user_bp.route('', methods=['POST'])
+@inject
+@auth_required
+def create_user(user_controller: UserController = Provide[Container.user_controller]):
+    user = UserCreateSchema(**request.json)
+    return jsonify(user_controller.create_user(user))
+
+
+@user_bp.route('/<uuid:user_id>', methods=['PUT'])
+@inject
+@auth_required
+def update_user(user_id: UUID, user_controller: UserController = Provide[Container.user_controller]):
+    user = UserUpdateSchema(**request.json)
+    return jsonify(user_controller.update_user(user_id, user))
+
+@user_bp.route('/<uuid:user_id>', methods=['DELETE'])
+@inject
+@auth_required
+def delete_user(user_id: UUID, user_controller: UserController = Provide[Container.user_controller]):
+    return jsonify(user_controller.delete_user(user_id))
---
+++ src/interface/web/controller/auth.py
@@ -0,0 +1,14 @@
+from typing import List
+from uuid import UUID
+
+from src.interface.web.schemas.user import (UserAuthSchema)
+from src.domain.entities.user import User
+from src.application.service.auth import AuthService
+
+
+class AuthController:
+    def __init__(self, auth_service: AuthService):
+        self.auth_service = auth_service
+
+    def login(self, user_auth: UserAuthSchema) -> dict:
+        return self.auth_service.verify_password(user_auth)
